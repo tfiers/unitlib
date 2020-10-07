61e7b69a3aa80d66ca7a727d294007fa1e44d8a1
Tomas Fiers <tomas.fiers@gmail.com>
2020-10-07 15:50:36 +0100

    🧪TEST str/repr/format

---
+++ tests/test_str_repr.py
@@ -0,0 +1,12 @@
+from yunit import Quantity, Unit
+
+
+def test_unit_str_repr():
+    volt = Unit.define('volt')
+    assert repr(volt) == '<DataUnitAtom "volt">'
+    assert str(volt) == f"{volt}" == "volt"
+
+def test_quantity_format():
+    volt = Unit.define('volt')
+    q =  Quantity(8, volt)
+    assert str(q) == repr(q) == f"{q}" == "8 volt"
