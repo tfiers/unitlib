08e2d1973d2108f5fe49b56d43c8f34fd007a03a
Tomas Fiers <tomas.fiers@gmail.com>
2020-11-04 13:16:44 +0100

    ⬆IMPR metric & binary prefixes

--- src/yunit/prefixes.py
+++ src/yunit/prefixes.py
@@ -1,16 +1,52 @@
+from abc import ABC
 from dataclasses import dataclass
 
 
 @dataclass
-class Prefix:
+class Prefix(ABC):
     symbol: str
-    factor: float
+    power: int
+    base = None
+
+    @property
+    def factor(self):
+        return self.base ** self.power
 
 
-kilo = Prefix("k", 1e3)
-deci = Prefix("d", 1e-1)
-centi = Prefix("c", 1e-2)
-milli = Prefix("m", 1e-3)
-micro = Prefix("μ", 1e-6)
-nano = Prefix("n", 1e-9)
-pico = Prefix("p", 1e-12)
+@dataclass
+class MetricPrefix(Prefix):
+    base = 10
+
+
+deca = MetricPrefix("da", 1)
+hecto = MetricPrefix("h", 2)
+
+kilo = MetricPrefix("k", 3)
+mega = MetricPrefix("M", 6)
+giga = MetricPrefix("G", 9)
+tera = MetricPrefix("T", 12)
+peta = MetricPrefix("P", 15)
+exa = MetricPrefix("E", 18)
+
+deci = MetricPrefix("d", -1)
+centi = MetricPrefix("c", -2)
+
+milli = MetricPrefix("m", -3)
+micro = MetricPrefix("μ", -6)
+nano = MetricPrefix("n", -9)
+pico = MetricPrefix("p", -12)
+femto = MetricPrefix("f", -15)
+atto = MetricPrefix("a", -18)
+
+
+@dataclass
+class BinaryPrefix(Prefix):
+    base = 1024
+
+
+kibi = BinaryPrefix("Ki", 1)
+mebi = BinaryPrefix("Mi", 2)
+gibi = BinaryPrefix("Gi", 3)
+tebi = BinaryPrefix("Ti", 4)
+pebi = BinaryPrefix("Pi", 5)
+exbi = BinaryPrefix("Ei", 6)
