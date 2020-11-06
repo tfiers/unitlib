c4ae0c25bd470956eda2a43ac4ab2eff2d27a036
Tomas Fiers <tomas.fiers@gmail.com>
2020-11-06 10:37:54 +0100

    📚DOC move "(No, `from . import *` doesn't work)." comment to end

--- src/yunit/ufunc_handling/__init__.py
+++ src/yunit/ufunc_handling/__init__.py
@@ -1,12 +1,11 @@
 # The modules in this package must all be imported, so the code in them is run, and the
-# methods in them are registered (via `@implements`). (No, `from . import *` doesn't
-# work).
+# methods in them are registered (via `@implements`).
 from . import (
     add_subtract,
     compare,
     divide,
     multiply,
     power,
-)
+) # (No, `from . import *` doesn't work).
 
 from .support import ufunc_handlers, UfuncArgs
