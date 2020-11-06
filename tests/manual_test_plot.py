62351e2095a12e642a504c158c8ea55d878df4e6
Tomas Fiers <tomas.fiers@gmail.com>
2020-11-06 14:16:44 +0100

    🔧MAINT do not auto-run mpl test

--- tests/manual_test_plot.py
+++ tests/manual_test_plot.py
@@ -9,7 +9,7 @@ ms = Unit.from_prefix(milli, second)
 volt = Unit.define("V")
 mV = Unit.from_prefix(milli, volt)
 
-# x = np.linspace(0, 10) * ms
-# y = np.random.randn(len(x)) * mV
+x = np.linspace(0, 10) * ms
+y = np.random.randn(len(x)) * mV
 
-# plt.plot(x, y)
+plt.plot(x, y)
