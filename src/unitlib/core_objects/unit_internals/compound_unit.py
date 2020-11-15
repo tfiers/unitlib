3f6e86961524a7cfe2377dcbc6ae9542a231b736
Tomas Fiers <tomas.fiers@gmail.com>
2020-11-15 17:18:07 +0100

    🔁REFACT move squeeze() from compound_unit.py to multiply.py

--- src/unitlib/core_objects/unit_internals/compound_unit.py
+++ src/unitlib/core_objects/unit_internals/compound_unit.py
@@ -2,6 +2,8 @@ from collections import defaultdict
 from itertools import chain
 from typing import Iterable, Dict, Tuple
 
+import numpy as np
+
 from unitlib.core_objects.unit import DataUnit, Unit
 from unitlib.support.backwards_compatibility import prod, TYPE_CHECKING
 
@@ -21,45 +23,15 @@ class CompoundUnit(Unit):
     def __init__(self, components: Tuple["PoweredUnitAtom", ...]):
         self.components = components
 
-    @classmethod
-    def squeeze(cls, units: Iterable["CompoundUnit"]) -> Unit:
-
-        from .unit_atom import UnitAtom, dimensionless
-
-        flattened_units = chain(*(unit.components for unit in units))
-
-        # Aggregate all `PoweredUnitAtom`s that have the same `unit_atom`, and sum their
-        # powers. Throw away dimensionless components and components with a combined
-        # power of zero.
-        combined_powers: Dict[UnitAtom, int] = defaultdict(lambda: 0)
-        for unit in flattened_units:
-            combined_powers[unit.unit_atom] += unit.power
-
-        squeezed_units = tuple(
-            unit_atom._raised_to(combined_power)
-            for unit_atom, combined_power in combined_powers.items()
-            if unit_atom != dimensionless and combined_power != 0
-        )
-
-        if len(squeezed_units) == 0:
-            return dimensionless
-
-        elif len(squeezed_units) == 1:
-            return squeezed_units[0]
-
-        else:
-            return cls(squeezed_units)
-            #   `cls` can be `CompoundUnit` or `CompoundDataUnit`.
-
     @property
     def name(self):
         return "·".join([component.name for component in self.components])
 
     @property
     def data_unit(self) -> "CompoundDataUnit":
-        return CompoundDataUnit.squeeze(
-            [component.data_unit for component in self.components]
-        )
+        # `np.prod` will automatically call our custom `np.multiply` implementation
+        # repeatedly (no need for us to implement `method="reduce"`).
+        return np.prod([component.data_unit for component in self.components])
 
     @property
     def scale(self) -> float:
@@ -69,7 +41,7 @@ class CompoundUnit(Unit):
         return hash(self.components)
 
     def _raised_to(self, power: int):
-        return self.squeeze([component ** power for component in self.components])
+        return np.prod([component ** power for component in self.components])
 
 
 class CompoundDataUnit(DataUnit, CompoundUnit):
--- src/unitlib/operations/multiply.py
+++ src/unitlib/operations/multiply.py
@@ -7,12 +7,15 @@ Examples:
   - 8 mV * 3
   - 8 mV * (mV ** -1)
 """
+from collections import defaultdict
+from typing import Dict
 
 import numpy as np
 
-from unitlib.core_objects import Unit, NonNumericDataException
+from unitlib.core_objects import Unit, NonNumericDataException, dimensionless
 from unitlib.core_objects.support.type_aliases import UfuncInput
-from unitlib.core_objects.unit_internals import CompoundUnit, DataUnitAtom, UnitAtom
+from unitlib.core_objects.unit import DataUnit
+from unitlib.core_objects.unit_internals import CompoundUnit, UnitAtom, CompoundDataUnit
 from unitlib.prefixes import Prefix
 from .support import make_binary_ufunc_output, UfuncOutput, implements, UfuncArgs
 
@@ -29,11 +32,9 @@ def multiply(args: UfuncArgs) -> UfuncOutput:
         else:
             raise exception
 
-    new_display_unit = CompoundUnit.squeeze(
-        [
-            inputs.left_array.display_unit,
-            inputs.right_array.display_unit,
-        ]
+    new_display_unit = combine(
+        inputs.left_array.display_unit,
+        inputs.right_array.display_unit,
     )
 
     # Unit * Unit remains pure Unit.
@@ -42,6 +43,37 @@ def multiply(args: UfuncArgs) -> UfuncOutput:
 
     else:
         return make_binary_ufunc_output(args, inputs, new_display_unit)
+
+
+def combine(left_unit: CompoundUnit, right_unit: CompoundUnit) -> Unit:
+
+    components = left_unit.components + right_unit.components
+
+    # Aggregate all `PoweredUnitAtom`s that have the same `unit_atom`, and sum their
+    # powers. Throw away dimensionless components and components with a combined
+    # power of zero.
+    combined_powers: Dict[UnitAtom, int] = defaultdict(lambda: 0)
+    for unit in components:
+        combined_powers[unit.unit_atom] += unit.power
+
+    squeezed_components = tuple(
+        unit_atom._raised_to(combined_power)
+        for unit_atom, combined_power in combined_powers.items()
+        if unit_atom != dimensionless and combined_power != 0
+    )
+
+    if len(squeezed_components) == 0:
+        return dimensionless
+
+    elif len(squeezed_components) == 1:
+        return squeezed_components[0]
+
+    else:
+        if all(isinstance(component, DataUnit) for component in squeezed_components):
+            cls = CompoundDataUnit
+        else:
+            cls = CompoundUnit
+        return cls(squeezed_components)
 
 
 def create_prefixed_unit(prefix: Prefix, right_operand: UfuncInput):
